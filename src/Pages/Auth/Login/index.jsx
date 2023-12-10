dd2dacbe48b96ea9e4efb00f2734a0c50b722857
Tuwns <manhtuan17403@gmail.com>
2023-12-10 19:07:38 +0700

    login screen

--- src/Pages/Auth/Login/index.jsx
+++ src/Pages/Auth/Login/index.jsx
@@ -1,12 +1,95 @@
-import React from "react";
-import {useAppDispatch, useAppSelector} from "../../../Store/store";
-import {useSelector} from "react-redux";
+import React from 'react';
+import {Button, Card, Checkbox, Divider, Form, Input, Space} from 'antd';
+import './styles.css'
 
 const Login = () => {
-  const {data}  = useAppSelector(state => state.app)
+  const onFinish = (values) => {
+    console.log('Success:', values);
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
   return (
-    <h1>{data}</h1>
+    <div className='container'>
+      <Space direction="vertical" size={20} >
+        <Card
+          title="Login"
+          style={{
+            width: 600,
+          }}
+        >
+          <Form
+            name="basic"
+            labelCol={{
+              span: 4,
+            }}
+            wrapperCol={{
+              span: 16,
+            }}
+            style={{
+              maxWidth: 600,
+            }}
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+          >
+            <Form.Item
+              label="Username"
+              name="username"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your username!',
+                },
+              ]}
+            >
+              <Input/>
+            </Form.Item>
+
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your password!',
+                },
+              ]}
+            >
+              <Input.Password/>
+            </Form.Item>
+
+            <Form.Item
+              name="remember"
+              valuePropName="checked"
+              wrapperCol={{
+                offset: 8,
+                span: 16,
+              }}
+            >
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+
+            <Form.Item
+              wrapperCol={{
+                offset: 8,
+                span: 16,
+              }}
+            >
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+        </Card>
+
+      </Space>
+    </div>
   )
-}
+
+};
 
 export default Login
